181e92dbe344d37d7104006c4d8bbf682f018610
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-02 21:02:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,6 +18,19 @@ class Triangle
    end
   end
 
+  def valid?
+  sum_one_two = @triangle_sides[0] + @triangle_sides[1]
+  sum_one_three = @triangle_sides[0] + @triangle_sides[2]
+  sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+
+  if (@triangle_sides.none? {|side| side <= 0}) &&
+    (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
+    return true
+  else
+    return false
+  end
+end
+
 class TriangleError < StandardError
 end
 end
